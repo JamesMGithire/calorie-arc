dfcda8ecf755ac03b69470cdedd1383abe3b265a
iSaacSigei <kiplangatisack.is@gmail.com>
2022-11-08 20:45:03 +0300

    CA-8 Adding NavLinks,signup,andlogin page with there respective css styling

---
+++ calorie_client/src/components/MealPlan.js
@@ -0,0 +1,11 @@
+import React from "react"
+function MealPlan(){
+    return(
+        <div>
+            <h1>
+            Meal plan section here
+            </h1>
+        </div>
+    )
+}
+export default MealPlan
---
+++ calorie_client/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <h2 style={{fontSize:"32px"}}>
+        <span style={{color:"red"}}>Cal</span><span style={{color:"white"}}>orie Arc</span>
+      </h2>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/meal_plan">Meal Plan</NavLink>
+      <NavLink to="/signup"><button className="button">Sign Up</button></NavLink>  
+    </div>
+  );
+}
+
+export default NavBar;
