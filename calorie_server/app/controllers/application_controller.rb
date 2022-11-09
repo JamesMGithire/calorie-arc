8fac0205c1da41b7837822f4ac3080bd442773f3
James Mburu Githire <jamesmburug24@gmail.com>
2022-11-09 20:57:18 +0300

    fixed typo, added  delete & post history,

--- calorie_server/app/controllers/application_controller.rb
+++ calorie_server/app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
     User.all.to_json
   end
   get '/meals' do
-    Meals.all.to_json
+    Meal.all.to_json
   end
 
   get '/cuisines' do
@@ -21,25 +21,40 @@ class ApplicationController < Sinatra::Base
   get '/history/:id' do
     begin
       History.find(params[:id]).to_json
-      
     rescue => exception
-      exception.to_json
+      {message: "Could not find history in databse with id #{params[:id]}\n"}.to_json
+    end
+  end
 
+  get '/choices/:user_id' do
+    begin
+      Choice.find_by(user_id: params[user_id])
+    rescue => exception
+      {message: "Could not find choices in databse for user_id #{params[user_id]}\n"}.to_json
     end
   end
   
   post '/history' do
     begin
-      History.create(meal_id: params[:meal_id], user_id:[:user_id]).to_json
+      History.create(meal_id: params[:meal_id], user_id: params[:user_id]).to_json
       rescue => exception
-        exception.to_json
+      {message: "Could not create history in databse\nERROR: #{exception.message}"}.to_json
       end
+  end
+
+  delete '/history/:id' do
+    begin
+      deleted = History.find(params[:id]).destroy
+      deleted.to_json
+    rescue => exception
+      {message: "Could not create history in databse\nERROR: #{exception.message}"}.to_json
+    end
   end
 
   post '/user' do
     begin
       User.create(username: params[:username],
-      first_name: params[:username],
+      first_name: params[:first_name],
       last_name: params[:lastname],
       calories: params[:calories],
       password: params[:password],
@@ -47,7 +62,7 @@ class ApplicationController < Sinatra::Base
       gender: params[:gender],
       ).to_json
     rescue => exception
-      {message: "Could not create user in databse\nERROR: #{exception.message}"}
+      {message: "Could not create user in databse\nERROR: #{exception.message}"}.to_json
     end
   end
 
