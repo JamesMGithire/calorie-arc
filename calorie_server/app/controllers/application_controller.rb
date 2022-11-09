a62a50b9e4c80072eee211a36f26267d00939d53
James Mburu Githire <jamesmburug24@gmail.com>
2022-11-09 13:38:08 +0300

    required environment and created home route

--- calorie_server/app/controllers/application_controller.rb
+++ calorie_server/app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+  get '/' do
+    
+  end
+  
   get '/users' do
     User.all.to_json
   end
@@ -18,22 +22,4 @@ class ApplicationController < Sinatra::Base
     
   end
 
-  post '/messages' do
-    message = Message.create(
-      body: params[:body],
-      username: params[:username]
-    )
-    message.to_json
-  end
-
-  patch '/messages/:id' do
-    message = Message.find(params[:id])
-    message.update(body: params[:body])
-    message.to_json
-  end
-
-  delete '/messages/:id' do
-    message = Message.find(params[:id]).destroy
-    message.to_json
-  end
 end
