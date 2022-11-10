5726b6bb2b5d0b681523c0e64faba36fe062dcce
James Mburu Githire <jamesmburug24@gmail.com>
2022-11-10 09:22:16 +0300

    Added African cuisine and seeded

--- calorie_server/db/seeds.rb
+++ calorie_server/db/seeds.rb
@@ -3,21 +3,21 @@ puts "Deleting..."
 # User.destroy_all()
 puts "🌱 Seeding ..."
 # User.create(username: "JKL")
-Cuisine.create(name: "African cuisine", img_url: "https://upload.wikimedia.org/wikipedia/commons/0/03/Alicha_1.jpg")
-Cuisine.create(name: "Asian cuisine", img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Vegetarian_Curry.jpeg/200px-Vegetarian_Curry.jpeg")
-Cuisine.create(name: "European cuisine", img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/250px-Wiener-Schnitzel02.jpg")
+# Cuisine.create(name: "African cuisine", img_url: "https://upload.wikimedia.org/wikipedia/commons/0/03/Alicha_1.jpg")
+# Cuisine.create(name: "Asian cuisine", img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Vegetarian_Curry.jpeg/200px-Vegetarian_Curry.jpeg")
+# Cuisine.create(name: "European cuisine", img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/250px-Wiener-Schnitzel02.jpg")
 
-Meal.create(category: "Breakfast", name:"Homemade Croissants",calories: 365, cuisine_id: 3, img_url: "https://bigseventravel.com/wp-content/uploads/2020/08/Screenshot-2020-08-06-at-10.15.34.png")
-Meal.create(category: "Breakfast", name:"Swiss Rosti With Smoked Salmon",calories: 589, cuisine_id: 3, img_url: "https://bigseventravel.com/wp-content/uploads/2020/08/Screenshot-2020-08-06-at-10.17.46.png")
-Meal.create(category: "Breakfast", name:"Racuszki! (Yeasted Polish Pancakes)",calories: 589, cuisine_id: 3, img_url: "https://bigseventravel.com/wp-content/uploads/2020/08/Screenshot-2020-08-06-at-10.23.04.png")
+Meal.create(category: "Breakfast", name:"Mandazi",calories: 160, cuisine_id: 1, img_url: "https://www.okayafrica.com/media-library/mandazi-are-like-an-african-donut-or-fritter.jpg?id=22775443&width=576&quality=80")
+Meal.create(category: "Breakfast", name:"Banana Biscotti",calories: 145, cuisine_id: 1, img_url: "https://i1.wp.com/thefoodexplorer.com/wp-content/uploads/2017/04/Banana-Biscotti-recipe.jpg?resize=756%2C1024")
+Meal.create(category: "Breakfast", name:"Crunchies (South African)",calories: 66, cuisine_id: 1, img_url: "https://i2.wp.com/thefoodexplorer.com/wp-content/uploads/2017/04/Crunchies-South-African-recipe.jpg?resize=768%2C1014")
 
-Meal.create(category: "Lunch", name:"Pierogi",calories: 230, cuisine_id: 3, img_url: "https://blog.topdeck.travel/wp-content/uploads/2017/04/Pierogi-1024x683.jpg")
-Meal.create(category: "Lunch", name:"Goulash",calories: 289, cuisine_id: 3, img_url: "https://blog.topdeck.travel/wp-content/uploads/2017/04/goulash-1024x683.jpg")
-Meal.create(category: "Lunch", name:"Köttbullar",calories: 589, cuisine_id: 3, img_url: "https://blog.topdeck.travel/wp-content/uploads/2017/04/kottbullar-1024x768.jpg")
+Meal.create(category: "Lunch", name:"Piri piri chicken, Mozambique",calories: 321, cuisine_id: 1, img_url: "https://dynaimage.cdn.cnn.com/cnn/q_auto,w_634,c_fill,g_auto,h_357,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F170410115320-africa-food---piri-piri-chicken.jpg")
+Meal.create(category: "Lunch", name:"Doro Wat",calories: 324, cuisine_id: 1, img_url: "https://www.okayafrica.com/media-library/image.jpg?id=22775422&width=700&quality=80")
+Meal.create(category: "Lunch", name:"Muamba de Galinha, Angola",calories: 541, cuisine_id: 1, img_url: "https://dynaimage.cdn.cnn.com/cnn/q_auto,w_634,c_fill,g_auto,h_357,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F170410115304-african-food---muamba-di-luanda.jpg")
 
-Meal.create(category: "Dinner", name:"Baked Cod in a Chorizo Broth",calories: 380, cuisine_id: 3, img_url: "https://images.ctfassets.net/qu53tdnhexvd/ldkjemMSFqcwCMIY0Qw88/db7deb84709c44474b93e894ad4b8903/baked-cod-with-olives-and-chorizo.jpg")
-Meal.create(category: "Dinner", name:"Eggs baked in patatas bravas",calories: 309, cuisine_id: 3, img_url: "https://img.taste.com.au/45N67qX3/w643-h428-cfill-q90/taste/2016/11/eggs-baked-in-patatas-bravas-100484-1.jpeg")
-Meal.create(category: "Dinner", name:"Köttbullar",calories: 589, cuisine_id: 3, img_url: "https://blog.topdeck.travel/wp-content/uploads/2017/04/kottbullar-1024x768.jpg")
+Meal.create(category: "Dinner", name:"Nyama na irio, Kenya",calories: 250, cuisine_id: 1, img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOkOuFkyUMv9wOBAQTmrVag8tiGxPcIe_bLw&usqp=CAU")
+Meal.create(category: "Dinner", name:"Jollof rice and egusi soup, Nigeria",calories: 284, cuisine_id: 1, img_url: "https://dynaimage.cdn.cnn.com/cnn/q_auto,w_634,c_fill,g_auto,h_357,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F170410115312-african-food---jollof-rice.jpg")
+Meal.create(category: "Dinner", name:"Ofada Stew",calories: 311, cuisine_id: 1, img_url: "https://www.okayafrica.com/media-library/nigerian-ofada-stew-with-rice-and-banana-leaf-and-egg.png?id=22775562&width=542&quality=80")
 # History.create(user_id: 5, meal_id: 2)
 
 puts "✅ Done seeding!"
