ae06ec1a4504c61257b688443f5bb5a104b3b213
James Mburu Githire <jamesmburug24@gmail.com>
2022-11-11 00:30:24 +0300

    indented for readability

--- calorie_client/src/components/ContactUs.js
+++ calorie_client/src/components/ContactUs.js
@@ -53,82 +53,82 @@ let handleSubmit = async (e) => {
 
 return (
     <section className='contact'>
-    <div className='content'>
-        <h2>Contact Us</h2>
-        <p>
-        Do you have a question about our products? We’d love to hear from you. Here’s how you can reach us..
-        </p>
-    </div>
-
-    <div className='container'>
-        <div className='contactInfo'>
-        <div className='box'>
-            <div className="icon">
-            < FaMapMarkerAlt/> 
-            </div>
-            <div className="text">
-            <h3>Address</h3>
-            <p> Ngong Lane, Ngong Lane Plaza, 1st Floor, Nairobi Kenya</p>
-            </div>          
-        </div>
-        <div className='box'>
-            <div className="icon">
-            <FaVoicemail/>  
-            </div>
-            <div className="text">
-            <h3>Email</h3>
-            <p> contact@caloriearc.com</p>
-            </div>          
-        </div>
-        <div className='box'>
-            <div className="icon">
-            <FaPhoneAlt />
-            </div>
-            <div className="text">
-            <h3>Phone</h3>
-            <p> (+254) 58-5674-578</p>
-            </div>          
-        </div>
+        <div className='content'>
+            <h2>Contact Us</h2>
+            <p>
+            Do you have a question about our products? We’d love to hear from you. Here’s how you can reach us..
+            </p>
         </div>
 
-        <div className='contactForm'>
-        <form action="" onSubmit={handleSubmit}>
-            <h2>Send Message</h2>
-            <div className='inputBox'>
-            <input 
-                type="text" 
-                name="" 
-                required="required" 
-                // value={state.fullname} 
-                onChange={onInputChange}/>
-            <label className="label" htmlFor='fullname'>Full Name</label>
+        <div className='container'>
+            <div className='contactInfo'>
+            <div className='box'>
+                <div className="icon">
+                < FaMapMarkerAlt/> 
+                </div>
+                <div className="text">
+                <h3>Address</h3>
+                <p> Ngong Lane, Ngong Lane Plaza, 1st Floor, Nairobi Kenya</p>
+                </div>          
+            </div>
+            <div className='box'>
+                <div className="icon">
+                <FaVoicemail/>  
+                </div>
+                <div className="text">
+                <h3>Email</h3>
+                <p> contact@caloriearc.com</p>
+                </div>          
+            </div>
+            <div className='box'>
+                <div className="icon">
+                <FaPhoneAlt />
+                </div>
+                <div className="text">
+                <h3>Phone</h3>
+                <p> (+254) 58-5674-578</p>
+                </div>          
+            </div>
             </div>
 
-            <div className='inputBox'>
-            <input type="text" 
-                name="" 
-                required="required" 
-                // value={state.email} 
-                onChange={onInputChange}/>
-            <label className="label" htmlFor='email'>Email</label>
-            </div>
+            <div className='contactForm'>
+            <form action="" onSubmit={handleSubmit}>
+                <h2>Send Message</h2>
+                <div className='inputBox'>
+                <input 
+                    type="text" 
+                    name="" 
+                    required="required" 
+                    // value={state.fullname} 
+                    onChange={onInputChange}/>
+                <label className="label" htmlFor='fullname'>Full Name</label>
+                </div>
 
-            <div className='inputBox'>
-            <textarea 
-                required="required" 
-                // value={state.message} 
-                onChange={onInputChange}>
-            </textarea>
-            <label className="label" htmlFor='message'>Type your message</label>
-            </div>
+                <div className='inputBox'>
+                <input type="text" 
+                    name="" 
+                    required="required" 
+                    // value={state.email} 
+                    onChange={onInputChange}/>
+                <label className="label" htmlFor='email'>Email</label>
+                </div>
 
-            <div className='inputBox'>
-            <input  type="submit" name="" value="Send"/>
+                <div className='inputBox'>
+                <textarea 
+                    required="required" 
+                    // value={state.message} 
+                    onChange={onInputChange}>
+                </textarea>
+                <label className="label" htmlFor='message'>Type your message</label>
+                </div>
+
+                <div className='inputBox'>
+                <input  type="submit" name="" value="Send"/>
+                </div>
+                
+            </form>
             </div>
-            
-        </form>
         </div>
-    </div>
     </section>
 )
 }
