ad4fcff82bb8fbfd15eac49e6433e6a3958890cd
littlephillips <phillipkinuthia72@gmail.com>
2022-11-10 14:11:30 +0300

    CA-24 added login page

--- calorie_client/src/components/Login.js
+++ calorie_client/src/components/Login.js
@@ -1,17 +1,50 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
 function Login({data}) {
   function handleChange(e){
     console.log(data)
     console.log(e.target.value)
   }
   return (
-    <div>
-      <form>
-        <input onChange={handleChange} type="text" placeholder="enter username/email"/>
-        <input onChange={handleChange} type="password" placeholder="password"/><br/>
-        <button>Login</button>
-      </form>
-      
+    <div className="background">
+      <div class="main-form">
+        <h1>Calorie-arc login</h1>
+        <form action="#" method="POST">
+            <div class="text-field">
+                <input 
+                type="text"
+                required="required"
+                onChange={handleChange}
+                />
+                <span></span>
+                <label>Name</label>
+            </div>
+
+            <div class="text-field">
+                <input
+                type="password"
+                required="required"
+                onChange={handleChange}
+                />
+                <span></span>
+                <label>Password</label>
+            </div>
+
+            <div class="pass">
+                Forgot Password?
+            </div>
+            <input
+            type="Submit"
+            value="Login"
+            />
+            <div class="signup_link">
+                Not a member?
+                <NavLink to='/signup'>
+                Signup</NavLink>
+            </div>
+        </form>
+    </div>
     </div>
   );
 }
