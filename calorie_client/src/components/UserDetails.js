b6028e8cd7ba1e476183b253b78b52e283f17ac1
littlephillips <phillipkinuthia72@gmail.com>
2022-11-13 03:52:25 +0300

    CA-27 updated form details

--- calorie_client/src/components/UserDetails.js
+++ calorie_client/src/components/UserDetails.js
@@ -1,9 +1,11 @@
-import React, { useState }from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
 function UserDetails({user, setUser}) {
+
   const nav = useNavigate();
   let userInfo={}
+
   function handleChange(e){
     userInfo={
       ...userInfo,
@@ -38,24 +40,12 @@ function UserDetails({user, setUser}) {
   return (
     <div className="background">
       <div className="main-form">
-        <h1>Enter your info to find diet</h1>
+        <h1>Enter your information</h1>
 
         <form onSubmit={handleSubmit}>
             <div className="text-field">
-                <input 
-                type="text"
-                required="required"
-                defaultValue=""
-                name="gender"
-                onChange={handleChange}
-                />
-                <span></span>
-                <label>Gender</label>
-            </div>
-
-            <div className="text-field">
                 <input
-                type="number"
+                type="number" min="12"
                 required="required"
                 defaultValue=""
                 name="age"
@@ -67,50 +57,57 @@ function UserDetails({user, setUser}) {
 
             <div className="text-field">
                 <input
-                type="number"
+                type="number" min="66"
                 defaultValue=""
                 required="required"
                 name="weight"
                 onChange={handleChange}
                 />
                 <span></span>
-                <label>Weight</label>
+                <label>Weight (lbs)</label>
             </div>
 
             <div className="text-field">
                 <input
-                type="text"
-                defaultValue=""
-                name="activity"
-                required="required"
-                onChange={handleChange}
-                />
-                <span></span>
-                <label>Activity</label>
-            </div>
-
-            <div className="text-field">
-                <input
-                type="number"
+                type="number" min="60"
                 defaultValue=""
                 required="required"
                 name="height"
                 onChange={handleChange}
                 />
                 <span></span>
-                <label>Height</label>
+                <label>Height (cm)</label>
             </div>
 
-            <div className="text-field">
-                <input
-                type="text"
-                defaultValue=""
-                required="required"
-                name="allergies"
-                onChange={handleChange}
-                />
-                <span></span>
-                <label>Allergy</label>
+            <div class="select-field">
+                <label>Gender:</label>
+              <select onChange={handleChange}>
+                <option  name ="male" value ="male"> Male</option>
+                <option  name ="female" value ="female">Female </option>
+                <option  name="other" value ="others"> Others</option>               
+              </select>
+            </div>
+
+            <div class="select-field">
+              <label>Activity:</label>
+              <select  onChange={handleChange}>
+                <option name="moderate" value ="moderate"> Moderate: exercise 4-5 times/week</option>
+                <option name="bmr" value ="bmr">Basal Metabolic Rate </option>
+                <option name="light" value ="light"> Light: exercise 1-3 times /week</option>
+                <option name="sedentary" value ="sedentary">Sedentary: little or no exercise </option>
+                <option name ="very" value ="active"> Very Active: exercise 6-7 times /week</option>                
+              </select>
+            </div>
+
+            <div class="select-field">
+              <label>Allergy:</label>
+              <select onChange={handleChange}>
+                <option name="anaphylaxis" value ="anaphylaxis"> Anaphylaxis</option>
+                <option name="milk" value ="milk-products">milk product allergy</option>
+                <option name="eggs" value ="eggs"> Egg allergy</option>
+                <option name="peanuts" value ="peanut">Peanut allergy </option>
+                <option name="wheat" value ="wheat"> wheat allergies</option>                
+              </select>
             </div>
 
             <input type="Submit" defaultValue="Submit"/>
