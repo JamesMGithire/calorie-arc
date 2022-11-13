68518d645d4a15d73694e899d6baccf8825aafd4
James Mburu Githire <jamesmburug24@gmail.com>
2022-11-13 03:02:25 +0300

    added meals

--- calorie_client/src/components/App.js
+++ calorie_client/src/components/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import React,{ useState} from "react";
+import React,{ useState, useEffect} from "react";
 import "../CSS/SignUp.css";
 import '../CSS/UserProfile/Index.css';
 import Login from "./Login";
@@ -17,14 +17,29 @@ function App() {
   const[newUser, setNewUser]=useState(false)
   const[user, setUser]=useState()
   const[loggedIn,setLoggedIn] = useState(false)
-  let meals = [];
+  let userMeals = [];
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:9292/meals")
+    .then(res=>res.json())
+    .then((data)=>{
+        setMeals(data)
+    })
+  },[]);
   if(loggedIn){
     fetch(`http://localhost:9292/choices/${user.id}`)
     .then(r=>r.json())
     .then(result=>{
-      console.log(result);
-      result.map(meal=>meals.push(meal.meal));
+      result.map(meal=>userMeals.push(meal.meal));
     })
+  }
+  const breakfasts= meals.filter(meal=>meal.category==="Breakfast");
+  const lunches = meals.filter(meal=>meal.category==="Lunch");
+  const dinners= meals.filter(meal=>meal.category==="Dinner");
+  const dayMeals = {
+    breakfast : (breakfasts[Math.floor((Math.random()*breakfasts.length))]),
+    lunch : (lunches[Math.floor((Math.random()*lunches.length))]),
+    dinner : (dinners[Math.floor((Math.random()*dinners.length))])
   }
   return (
     <div className="App">
@@ -32,15 +47,15 @@ function App() {
       <Routes>
         <Route  path="/login" element={<Login setLoggedIn={setLoggedIn} setUser={setUser}/>}/>
         <Route path="/signup" element={<SignUp setUser={setUser} setLoggedIn={setLoggedIn}/>}/>
+        <Route path="/meals" element={<CuisinePage meals={meals} user={user} loggedIn={loggedIn}/>}/>
         { loggedIn &&
           <>
-          <Route path="/userprofile" element={<UserProfile user={user} setUser={setUser} setLoggedIn={setLoggedIn}/>}/>
-          <Route path="/meals" element={<CuisinePage user={user} loggedIn={loggedIn}/>}/>
+          <Route path="/userprofile" element={<UserProfile dayMeals={dayMeals} user={user} setUser={setUser} setLoggedIn={setLoggedIn}/>}/>
           <Route path="/user_details" element={<UserDetails user={user} setUser={setUser}/>}/>
         </>
         }
         <Route path="/contact_us" element={<ContactUs/>}/>
-        <Route path="/meal_plan" element={<UserMeals meals={meals}/>}/>
+        <Route path="/meal_plan" element={<UserMeals userMeals={userMeals}/>}/>
         <Route path="/" element={<LandingPage/>}/>
       </Routes>
       <Footer/>
