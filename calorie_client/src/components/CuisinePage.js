01dfe1942aee8ec29eb9e73c4d039a616df66781
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-11-13 06:24:22 +0300

    Added styling

--- calorie_client/src/components/CuisinePage.js
+++ calorie_client/src/components/CuisinePage.js
@@ -1,50 +1,84 @@
 import React, { useEffect, useState } from "react";
 
-function CuisinePage({user, loggedIn}) {
+function CuisinePage({ user, loggedIn }) {
   const [meals, setMeals] = useState([]);
   useEffect(() => {
     fetch("http://localhost:9292/meals")
-    .then(res=>res.json())
-    .then((data)=>{
-        setMeals(data)
-    })
-  },[]);
-  function addChoices(cuisineId){
-    fetch("http://localhost:9292/choices",{
+      .then((res) => res.json())
+      .then((data) => {
+        setMeals(data);
+      });
+  }, []);
+  function addChoices(cuisineId) {
+    fetch("http://localhost:9292/choices", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({meal_id: cuisineId, user_id: user.id})
+      body: JSON.stringify({ meal_id: cuisineId, user_id: user.id }),
     })
-    .then(r=>r.json())
-    .then(re=>console.log(re))
+      .then((r) => r.json())
+      .then((re) => console.log(re));
   }
   return (
     <div className="cuisine_page">
-        <h1>AVAILABLE MEALS</h1>
-        <label className="cuisines">Choose Cuisine</label>
-        <select>
-          
-        </select>
-        <div className="cuisine_header">
-            <div><h3>Images</h3></div>
-            <div><h3> Name</h3></div>
-            <div><h3> Category</h3></div>
-            <div><h3>Calories</h3></div>
-            <div><h3>Choose Meal</h3></div>
+      <h1>AVAILABLE MEALS</h1>
+      <div className="choices-cuisines">
+        <button className="cuisines">Choose Cuisine</button>
+        <div className="options-cuisines">
+          <select></select>
         </div>
-           {meals.map((cus)=>(
-            <div key={cus.id} className="cuisine_card">
-                <div className="tr"><img src={cus.img_url}/></div>
-                <div className="tr"><h4>{cus.name}</h4></div>
-                <div className="tr"><h4>{cus.category}</h4></div>
-                <div className="tr"><h4>{cus.calories}</h4></div>
-              {loggedIn && <div className="tr"><button onClick={()=>addChoices(cus.id)} style={{backgroundColor:"red", color:"white", width:"50px", height:"40px",borderRadius:"8px", fontSize:"24px"}}>+</button></div>}  
+      </div>
+      <div className="cuisine_header">
+        <div>
+          <h3>Images</h3>
+        </div>
+        <div>
+          <h3> Name</h3>
+        </div>
+        <div>
+          <h3> Category</h3>
+        </div>
+        <div>
+          <h3>Calories</h3>
+        </div>
+        <div>
+          <h3>Choose Meal</h3>
+        </div>
+      </div>
+      {meals.map((cus) => (
+        <div key={cus.id} className="cuisine_card">
+          <div className="tr">
+            <img src={cus.img_url} />
+          </div>
+          <div className="tr">
+            <h4>{cus.name}</h4>
+          </div>
+          <div className="tr">
+            <h4>{cus.category}</h4>
+          </div>
+          <div className="tr">
+            <h4>{cus.calories}</h4>
+          </div>
+          {loggedIn && (
+            <div className="tr">
+              <button
+                onClick={() => addChoices(cus.id)}
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                  width: "50px",
+                  height: "40px",
+                  borderRadius: "8px",
+                  fontSize: "24px",
+                }}
+              >
+                +
+              </button>
             </div>
-           ))
-           }
-
+          )}
+        </div>
+      ))}
     </div>
   );
 }
